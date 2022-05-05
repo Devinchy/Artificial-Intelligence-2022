7b5f281520d4c4bd575016e9ed6353c72dd1d225
100451207 <100451207@alumnos.uc3m.es>
2022-05-05 18:31:15 +0200

    probabilidad intento

--- Paloma_branch/mdp_main_paloma.py
+++ Paloma_branch/mdp_main_paloma.py
@@ -2,5 +2,16 @@
 
 import csv
 
-with open("Data (2).csv") as f:
+count = 0
+count_rows = 0
+with open("Data.csv") as f:
     reader = csv.reader(f)
+    for row in reader:
+        count_rows += 1
+        if row == ['High;High;Low;E;High;High;Low']:
+            count += 1
+    print(count)
+    print(count_rows)
+"""P(s'= H,H,L| s = H,H,L , a = E)"""
+probability = count/count_rows
+print(probability)
