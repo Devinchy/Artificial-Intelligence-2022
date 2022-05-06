3fcab2424e08eb446a1fe4e6e90f0024c7039164
Devinchy <74410292+Devinchy@users.noreply.github.com>
2022-05-06 17:30:34 +0200

    Cambios importantes en la estructura del trabajo-parte 2

--- Source/main.py
+++ Source/main.py
@@ -6,16 +6,18 @@ with open("../Data_files/Data.csv") as f:
 
 my_agents = 3
 my_states_list = ["High", "Low"]
+my_init_states = ["High;High;High", "High;High;Low", "High;Low;High",
+                                 "High;Low;Low", "Low,High,High", "Low,High,Low",
+                                 "Low,Low,High", "Low,Low,Low"]
 my_action_list = ["E", "W", "N"]
 my_costs = {"C(E)": 3, "C(W)": 3, "C(N)": 3}
 
 
-my_init_states = ["High;High;High", "High;High;Low", "High;Low;High",
-                                 "High;Low;Low", "Low,High,High", "Low,High,Low",
-                                 "Low,Low,High", "Low,Low,Low"]
-
 MyMDP = Traffic_Lights_MDP(reader, my_states_list, my_init_states, my_agents, my_action_list, my_costs)
 
+probability_data = MyMDP.calculate_all_probabilities()
+
+print(probability_data)
 
 #lista_de_estados = {"High;High;High": {}, "High;High;Low": {},"High;Low;High": {},
                                  #"High;Low;Low": {}, "Low,High,High": {}, "Low,High,Low": {},
--- Source/traffic_lights_mdp_class.py
+++ Source/traffic_lights_mdp_class.py
@@ -8,8 +8,24 @@ from Probability_methods.state_low_low_high import probability_low_low_high
 from Probability_methods.state_low_low_low import probability_low_low_low
 
 
-class Traffic_Lights_MDP:
+class MDP:
     # attributes definition of the class
+
+    def __init__(self,my_data, state_list, initstates_list, elements, action_list, costs_dict):
+
+        self.data_file = my_data
+        self.states = state_list
+        self.initial_states_list = initstates_list
+        self.agents = elements
+        self.actions = action_list
+        self.costs = costs_dict
+
+class Traffic_Lights_MDP(MDP):
+
+    def __init__(self, my_data, state_list, initstates_list, elements, action_list, costs_dict):
+        super().__init__(my_data, state_list, initstates_list, elements, action_list, costs_dict)
+
+        self.probabilities
 
     # METHODS DEFINITION: PROBABILITY CALCULATION OF STATES
     # assignment from external files:
@@ -22,5 +38,9 @@ class Traffic_Lights_MDP:
     calculate_probability_LHL = probability_low_high_low()
     calculate_probability_LLH = probability_low_low_high()
     calculate_probability_LLL = probability_low_low_low()
+
+    def calculate_all_probabilities(self):
+
+        self.probabilities.update({})
 
     #METHODS DEFINITION: BELLMAN EQUATIONS AND OPTIMAL POLICY CALCULATION
